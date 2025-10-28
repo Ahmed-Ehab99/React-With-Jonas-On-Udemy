ea2641555c13912307123bf93391f42a0fe535fb
Ahmed <ahmedehab1677@gmail.com>
2025-10-29 01:31:52 +0300

    The Wild Oasis Dashboard

---
+++ the-wild-oasis/src/features/cabins/useDeleteCabin.js
@@ -0,0 +1,23 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { deleteCabin as deleteCabinApi } from "../../services/apiCabins";
+
+const useDeleteCabin = () => {
+  const queryClient = useQueryClient();
+
+  const { isPending: isDeleting, mutate: deleteCabin } = useMutation({
+    mutationKey: ["delete-cabin"],
+    mutationFn: (id) => deleteCabinApi(id),
+    onSuccess: () => {
+      toast.success("Cabin Successfully Deleted!");
+      queryClient.invalidateQueries({
+        queryKey: ["cabins"],
+      });
+    },
+    onError: (error) => toast.error(error.message),
+  });
+
+  return { isDeleting, deleteCabin };
+};
+
+export default useDeleteCabin;
